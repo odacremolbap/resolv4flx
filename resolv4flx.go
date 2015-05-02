d6e2926f8c61ce21a331b082e9d6e58c7f460ee8
= <=>
2015-05-02 23:03:50 +0200

    Desarrollo base. Falta sincronizar la salida.

--- resolv4flx.go
+++ resolv4flx.go
@@ -1,13 +1,93 @@
 package main
 
-import( "fmt")
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"sync"
+)
+
+var (
+	workers       int
+	fileName      string
+	waitResolvers sync.WaitGroup
+)
+
+func init() {
+	flag.IntVar(&workers, "workers", 5, "Number of worker threads to resolve DNS entries")
+
+}
 
 func main() {
-  // TODO check args
 
-  // TODO start process
-  
-  // TODO sync and end process
-  
-  fmt.Println("Finished processing")
+	//check args
+	argsCheck()
+
+	// Launch DNS resolvers
+	ch := make(chan string)
+	for i := 0; i < workers; i++ {
+		go resolveEntry(ch)
+	}
+
+	// Launch file reader
+	waitResolvers.Add(1)
+	readEntriesFile(fileName, ch)
+
+	// Exit when when reader and resolvers are done
+	waitResolvers.Wait()
+
+	fmt.Println("Finished processing")
+}
+
+func usage() {
+	// TODO print usage
+	fmt.Println("Utility to resolve DNS entries in a file")
+	os.Exit(2)
+
+}
+
+// Check command-line arguments
+func argsCheck() {
+
+	flag.Usage = usage
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) != 1 {
+		usage()
+	}
+
+	fileName = args[0]
+
+}
+
+func readEntriesFile(fileName string, ch chan string) {
+
+	// decrement wait count on exit
+	defer waitResolvers.Done()
+
+	fhandler, err := os.Open(fileName)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(2)
+	}
+
+	defer fhandler.Close()
+
+	scanner := bufio.NewScanner(fhandler)
+
+	for scanner.Scan() {
+
+		if scanner.Text() != "" {
+			// add wait count for each file entry
+			waitResolvers.Add(1)
+			ch <- scanner.Text()
+		}
+	}
+
+	close(ch)
+
 }
---
+++ resolver.go
@@ -0,0 +1,196 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"strings"
+)
+
+func resolveEntry(ch chan string) {
+
+	var response string
+	var err error
+
+	for {
+		entry := <-ch
+		if entry == "" {
+			// if nothing was sent through channel, loop again
+			continue
+		}
+
+		domain, entryType := parseLine(entry)
+
+		switch entryType {
+		case "A", "AAAA":
+			response, err = resolveA(domain)
+		case "PTR":
+			response, err = resolvePTR(domain)
+		case "MX":
+			response, err = resolveMX(domain)
+		case "SRV":
+			response, err = resolveSRV(domain)
+		default:
+			response = ""
+			err = errors.New("Lookup for " + entryType + " not supported")
+		}
+
+		printResult(domain, entryType, response, err)
+
+		waitResolvers.Done()
+	}
+
+}
+
+func parseLine(entry string) (string, string) {
+
+	var domain string
+	entryFields := strings.Split(entry, "\t")
+
+	if entryFields[0][len(entryFields[0])-1:] == "." {
+		domain = entryFields[0][:len(entryFields[0])-1]
+	} else {
+		domain = entryFields[0]
+	}
+
+	return domain, entryFields[1]
+}
+
+func resolveA(domain string) (string, error) {
+
+	ips, err := net.LookupIP(domain)
+
+	if err != nil {
+		return "", err
+	}
+
+	//TODO build string using buffers
+	var header string
+	response := ""
+
+	for _, ip := range ips {
+
+		if ip.To4() == nil {
+			header = "IPv6:"
+		} else {
+			header = "IPv4:"
+		}
+		response += "[" + header + ip.String() + "]"
+	}
+
+	return response, nil
+}
+
+func resolveMX(domain string) (string, error) {
+
+	mxs, err := net.LookupMX(domain)
+
+	if err != nil {
+		return "", err
+	}
+
+	//TODO build string using buffers
+	response := ""
+
+	for _, mx := range mxs {
+		response += "[host:" + mx.Host + ",pref:" + string(mx.Pref) + "]"
+	}
+
+	return response, nil
+}
+
+func resolvePTR(domain string) (string, error) {
+
+	//reverse ip octects and remove '.in-addr.arpa'
+	ptrChunks := strings.Split(domain, ".")
+
+	//allocate space
+	bufIP := make([]byte, len(ptrChunks[0])+len(ptrChunks[1])+len(ptrChunks[2])+len(ptrChunks[3])+3)
+	pointer := 0
+
+	pointer = copy(bufIP, ptrChunks[3])
+	pointer += copy(bufIP[pointer:], ".")
+	pointer += copy(bufIP[pointer:], ptrChunks[2])
+	pointer += copy(bufIP[pointer:], ".")
+	pointer += copy(bufIP[pointer:], ptrChunks[1])
+	pointer += copy(bufIP[pointer:], ".")
+	copy(bufIP[pointer:], ptrChunks[0])
+
+	domain = string(bufIP)
+
+	names, err := net.LookupAddr(domain)
+
+	if err != nil {
+		return "", err
+	}
+
+	//TODO build string using buffers
+	response := ""
+
+	for _, name := range names {
+		response += "[name:" + name + "]"
+	}
+
+	return response, nil
+
+}
+
+func resolveSRV(domain string) (string, error) {
+
+	var serviceIndex int
+	var protocolIndex int
+
+	// extract service and protocol
+	serviceIndex = strings.Index(domain, ".")
+
+	if serviceIndex != -1 {
+		protocolIndex = strings.Index(domain[serviceIndex+1:], ".") + serviceIndex
+	}
+
+	if serviceIndex == -1 || protocolIndex < serviceIndex {
+		return "", errors.New("SRV query '" + domain + "' should be formated as 'service.protocol.domain'")
+	}
+
+	service := domain[:serviceIndex]
+	protocol := domain[serviceIndex+1 : protocolIndex+1]
+
+	// remove underscore
+
+	if service[0:1] == "_" {
+		service = service[1:]
+	}
+
+	if protocol[0:1] == "_" {
+		protocol = protocol[1:]
+	}
+
+	cname, srvs, err := net.LookupSRV(service, protocol, domain[protocolIndex+2:])
+
+	if err != nil {
+		return "", err
+	}
+
+	//TODO build string using buffers
+	response := "cname:" + cname
+
+	for _, srv := range srvs {
+		response += "[target:" + srv.Target + ",port:" + string(srv.Port) + ",priority:" + string(srv.Priority) + ",weight:" + string(srv.Weight) + "]"
+	}
+
+	return response, err
+}
+
+func printResult(query string, entryType string, response string, err error) {
+	// TODO sync
+
+	fmt.Println("----------------------------------")
+	fmt.Println("Query: " + query)
+	fmt.Println("Type: " + entryType)
+
+	if err != nil {
+		fmt.Println("Error: " + err.Error())
+	} else {
+		fmt.Println("Response: " + response)
+	}
+
+}
